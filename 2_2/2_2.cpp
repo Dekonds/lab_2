aef44aa6f511f0bb4587225f36cd40e12cc21bc9
Dekonds <And.Dekond@gmail.com>
2021-09-30 20:43:45 +0300

    asdf
    Signed-off-by: Dekonds <And.Dekond@gmail.com>

---
+++ 2_2/2_2.cpp
@@ -0,0 +1,28 @@
+﻿#include<string>
+#include<iostream>
+#include<Windows.h> 
+using namespace std;
+
+void main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	string Line = "", word = "";
+	string abobus = "string";
+	bool Aa = false;
+	printf("Введите текст: ");
+	getline(cin, Line);
+	for (int i = 0; i < Line.length(); i++)
+	{
+		word += Line[i];
+		if (Line[i] == 'а' || Line[i] == 'А')
+			Aa = true;
+		if (Line[i] == ' ' || Line[i + 1] == '\0')
+		{
+			if (Aa)
+				cout << word;
+			word = "";
+			Aa = false;
+		}
+	}
+}
